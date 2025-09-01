462b39cd4586849ebf6724d7db62945c886c245d
AdityaP <pathak.aditya.in@gmail.com>
2025-08-31 22:05:27 -0500

    Finished header and main file

--- NowWhereDidIPutThat.cpp
+++ NowWhereDidIPutThat.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include "strextra.h"
 using namespace std;
 
 
@@ -8,9 +9,25 @@ using namespace std;
 
 int main() {
 
+    std::string s = "The quick brown fox";
 
+    assert(strextra::find(s, 'e') == 2);              
+    assert(strextra::find(s, std::string("e")) == 2);  
+    assert(strextra::find(s, ' ') == 3);              
+    assert(strextra::find(s, std::string("quick")) == 4);  
+    assert(strextra::find(s, std::string("quiet")) == -1); 
+    assert(strextra::find(s, std::string("cow")) == -1);  
+    assert(strextra::find(s, std::string("hix")) == -1);  
 
+    std::string s2 = "11112";
 
+    assert(strextra::find(s2, std::string("112")) == 2); 
+
+    strextra::find(s, "deer");
+
+    strextra::find(s,'e');
+
+    strextra::find(s2,'3');
 
 
 
--- strextra.h
+++ strextra.h
@@ -1,3 +1,82 @@
 
 
-string 
+#ifndef STREXTRA_H
+#define STREXTRA_H
+
+#include <string>
+using namespace std;
+
+namespace strextra {
+
+
+    int find(string str, char a) {
+
+        for (int i = 0; i < str.size(); i++) {
+            if (str[i] == a) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    int find(string str, string substring) {
+
+        if(substring.empty() || substring.size() > str.size()) {
+            return -1;
+        }
+        
+        if(substring.size() == str.size()) {
+            if(str == substring) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+
+        for (int i = 0; i <= str.size() - substring.size(); i++) {
+            if (str.substr(i, substring.size()) == substring) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif 
